82fbfbe2e3573b8536ddad3d1b8bd72a5c3723cf
Puzzak <puzzak69@gmail.com>
2020-06-01 06:29:35 +0300

    Create 1answerCallbackQuery.php

---
+++ Methods/1answerCallbackQuery.php
@@ -0,0 +1,7 @@
+            Request(
+                "answerCallbackQuery",
+                array(
+                    "callback_query_id"=>$id,
+                    "text"=>"Message on top of screen"
+                    )
+                );
